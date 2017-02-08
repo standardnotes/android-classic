aeba23cf325666ca18a4c2283afe914515e8b274
herbig <mherbig@barkbox.com>
2017-02-07 21:16:57 -0500

    - app icon (using Roman Nurik icon generator) - general usability issues on login (move focus down from keyboard, disable fields while processing request, add padding to password confirm)

--- app/src/main/java/org/standardnotes/notes/LoginActivity.kt
+++ app/src/main/java/org/standardnotes/notes/LoginActivity.kt
@@ -1,23 +1,26 @@
 package org.standardnotes.notes
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import org.standardnotes.notes.comms.Crypt
 import org.standardnotes.notes.comms.data.AuthParamsResponse
 import org.standardnotes.notes.comms.data.SigninResponse
+import org.standardnotes.notes.store.ValueStore
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import android.widget.LinearLayout
-import android.widget.EditText
-
 
 
 /**
@@ -25,8 +28,7 @@ import android.widget.EditText
  */
 class LoginActivity : AppCompatActivity() {
 
-
-
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -40,18 +42,18 @@ class LoginActivity : AppCompatActivity() {
                 } else {
                     Toast.makeText(this@LoginActivity, getString(R.string.error_login), Toast.LENGTH_LONG).show()
                 }
-                login_progress.visibility = View.GONE
+                hideProgress()
             }
 
             override fun onFailure(call: Call<SigninResponse>, t: Throwable) {
                 Toast.makeText(this@LoginActivity, getString(R.string.error_login), Toast.LENGTH_LONG).show()
-                login_progress.visibility = View.GONE
+                hideProgress()
             }
         }
 
         email_sign_in_button.setOnClickListener {
             try {
-                login_progress.visibility = View.VISIBLE
+                showProgress()
                 SApplication.instance!!.valueStore.server = server.text.toString()
                 SApplication.instance!!.resetComms()
                 SApplication.instance!!.comms.api.getAuthParamsForEmail(email.text.toString()).enqueue(object : Callback<AuthParamsResponse> {
@@ -59,7 +61,7 @@ class LoginActivity : AppCompatActivity() {
                         try {
                             val params = response.body()
                             if (!Crypt.isParamsSupported(this@LoginActivity, params)) {
-                                login_progress.visibility = View.GONE
+                                hideProgress()
                                 return
                             }
                             Crypt.doLogin(email.text.toString(), password.text.toString(), params, signInCallback)
@@ -72,13 +74,13 @@ class LoginActivity : AppCompatActivity() {
 
                     override fun onFailure(call: Call<AuthParamsResponse>, t: Throwable) {
                         Toast.makeText(this@LoginActivity, getString(R.string.error_login), Toast.LENGTH_LONG).show()
-                        login_progress.visibility = View.GONE
+                        hideProgress()
                     }
                 })
             } catch (e: Exception) {
                 e.printStackTrace()
                 Toast.makeText(this@LoginActivity, getString(R.string.error_login), Toast.LENGTH_LONG).show()
-                login_progress.visibility = View.GONE
+                hideProgress()
             }
         }
         sign_up.setOnClickListener {
@@ -86,26 +88,103 @@ class LoginActivity : AppCompatActivity() {
                 SApplication.instance!!.valueStore.server = server.text.toString()
                 SApplication.instance!!.resetComms()
 
-                val input = LayoutInflater.from(this).inflate(R.layout.view_password_confirm, null, false) as EditText
-                AlertDialog.Builder(this).setTitle(R.string.prompt_confirm_password)
+                val layout = LayoutInflater.from(this).inflate(R.layout.view_password_confirm, null, false)
+                val input = layout.findViewById(R.id.password) as EditText
+                val dialog = AlertDialog.Builder(this).setTitle(R.string.prompt_confirm_password)
                         .setNegativeButton(R.string.action_cancel, null)
                         .setPositiveButton(R.string.action_ok, { dialogInterface, i ->
                             if (input.text.toString() == password.text.toString()) {
-                                login_progress.visibility = View.VISIBLE
+                                showProgress()
                                 Crypt.doRegister(email.text.toString(), password.text.toString(), signInCallback)
                             } else {
                                 Toast.makeText(this@LoginActivity, R.string.error_passwords_mismatch, Toast.LENGTH_LONG).show()
                             }
                         })
-                        .setView(input)
+                        .setView(layout)
                         .show()
+                dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+                val okbutton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                input.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                        //
+                    }
 
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                        okbutton.isEnabled = !input.text.isBlank()
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        //
+                    }
+                })
+                input.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+                    if (actionId == EditorInfo.IME_ACTION_DONE && okbutton.isEnabled) {
+                        okbutton.performClick()
+                        return@OnEditorActionListener true
+                    }
+                    false
+                })
+                okbutton.isEnabled = false
             } catch (e: Exception) {
                 e.printStackTrace()
                 Toast.makeText(this@LoginActivity, getString(R.string.error_login), Toast.LENGTH_LONG).show()
-                login_progress.visibility = View.GONE
+                hideProgress()
             }
         }
+
+        password.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE && email_sign_in_button.isEnabled) {
+                email_sign_in_button.performClick()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkValidInput()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+        }
+        server.addTextChangedListener(textWatcher)
+        email.addTextChangedListener(textWatcher)
+        password.addTextChangedListener(textWatcher)
+
+        server.setText(ValueStore(this).server)
+        email.requestFocus()
     }
+
+    private fun checkValidInput() {
+        val valid: Boolean = !server.text.isBlank() && !email.text.isBlank() && !password.text.isBlank()
+        email_sign_in_button.isEnabled = valid
+        sign_up.isEnabled = valid
+    }
+
+    private fun showProgress() {
+        // TODO lock orientation changes?
+        server.isEnabled = false
+        email.isEnabled = false
+        password.isEnabled = false
+        email_sign_in_button.isEnabled = false;
+        sign_up.isEnabled = false
+        login_progress.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        // TODO unlock orientation changes?
+        server.isEnabled = true
+        email.isEnabled = true
+        password.isEnabled = true
+        checkValidInput()
+        login_progress.visibility = View.GONE
+    }
+
 }
 
