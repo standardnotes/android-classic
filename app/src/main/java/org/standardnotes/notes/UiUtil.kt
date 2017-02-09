334aba70d80985048ba64d30ed5485ef9e98798b
Konstantin Vorobyev <vorobyev@coacha.com>
2017-02-09 15:32:29 +0900

    Replace custom divider with RecyclerView provided one

--- app/src/main/java/org/standardnotes/notes/UiUtil.kt
+++ app/src/main/java/org/standardnotes/notes/UiUtil.kt
@@ -1,8 +1,2 @@
 package org.standardnotes.notes
 
-import android.util.TypedValue
-
-fun Int.dpToPixels(): Int {
-    val metrics = SApplication.instance!!.resources.displayMetrics
-    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
-}
