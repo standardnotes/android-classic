9a46f2a54ba5bb35fcffcc0927752670a98f1ba2
Carl Fletcher <carl@deftelf.co.uk>
2017-02-09 10:56:37 +0000

    Restore doToPixels()

--- app/src/main/java/org/standardnotes/notes/UiUtil.kt
+++ app/src/main/java/org/standardnotes/notes/UiUtil.kt
@@ -1,2 +1,8 @@
 package org.standardnotes.notes
 
+import android.util.TypedValue
+
+fun Int.dpToPixels(): Int {
+    val metrics = SApplication.instance!!.resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
+}
